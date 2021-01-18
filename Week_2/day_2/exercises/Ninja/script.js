0bcd0cd4f38224f06063918c5f6106211baec413
mikikop <michael@cinewan.com>
2021-01-18 17:20:22 +0200

    week2day2Ninja

--- Week_2/day_2/exercises/Ninja/script.js
+++ Week_2/day_2/exercises/Ninja/script.js
@@ -1,43 +1,38 @@
-//Ex 1
-let x = 12;
-let y = 11;
+// //Ex1
+// let date1 = prompt('give me the first year of birth in the format YYYY');
+// let date2 = prompt('give me the second year of birth in the format YYYY');
+// let differenceAge;
+// let dateFirst = Number(date1);
+// let dateSecond = Number(date2);
 
-if (x>y) {
-    console.log(x + " is bigger than "+ y);
-} else if (x<y) {
-    console.log(y + " is bigger than "+ x);
-}else {
-    console.log(x + " is equal to "+ y);
-}
+// if (dateFirst > dateSecond) {
+//     differenceAge = dateFirst - dateSecond;
+//     let dateHalf = dateFirst + differenceAge;
+//     alert('the younger one is exactly half the age of the other in the year ' + dateHalf);
+// } else {
+//     differenceAge = dateSecond - dateFirst;
+//     let dateHalf = dateSecond + differenceAge;
+//     alert('the younger one is exactly half the age of the other in the year ' + dateHalf);   
+// }
 
-//Ex2
-let newDog = "Chihuahua";
-let counter = newDog.length;
-console.log("There are " + counter + " letters in "+ newDog);
-console.log(newDog.toUpperCase());
-console.log(newDog.toLowerCase());
-if (newDog === "Chihuahua") {
-    console.log("I love Chihuahua, it’s my favorite dog breed");
-}else {
-    console.log("I dont care, I prefer cats");   
-}
+// //Ex2
+// let zipCode = prompt ('enter your zip code, please: (5 digits only not more, no space)');
+
+// // let patt = new RegExp("d{5}");
+// // let res = patt.test(zipCode);
+
+// let res = /\d{5}/.test(zipCode);
+
+// alert(res);
+
+// if (res == true){
+//     alert('Your zipcode has the good format');
+// }else {
+//     alert('Your zipcode has a wrong format');
+// }
 
 //Ex3
-let num = prompt("Please enter a number ! ");
-if (num%2 == 0) {
-    alert(num + " is an even number");
-} else {
-    alert(num + " is not an even number");
-}
+let word = prompt ('enter a word');
 
-//Ex4
-let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
-if (!(Array.isArray(users) && users.length)) {
-    console.log("no one is online");
-}else if (users.length == 1) {
-    console.log(users[0] + " is online");
-} else if (users.length == 2) {
-    console.log(users[0] + " and " + users[1] + " are online");
-} else {
-    console.log(users[0] + ", " + users[1] + " and " + ((users.length) - 2) + " more are online");
-}
+const regex = /aeiou/i;
+console.log(word.replace(regex, ''));
