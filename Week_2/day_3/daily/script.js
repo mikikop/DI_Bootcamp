b252ba59d2f55025b61679cf92e528e310e5b7be
mikikop <michael@cinewan.com>
2021-01-19 18:40:03 +0200

    week2day3 Daily Stars

--- Week_2/day_3/daily/script.js
+++ Week_2/day_3/daily/script.js
@@ -1,2 +1,17 @@
+let answer = prompt('How many lines of stars do you want?');
+let starLine = Number(answer);
 
-console.log("*");
+let tabStar = [];
+
+
+for (let i = 0; i < starLine ; i++) {
+  var starString = '';
+  for (let j=0; j<i+1 ; j++){
+    starString = starString + '*';
+  }
+  tabStar[i] = starString;
+}
+
+for (let i=0;i<tabStar.length;i++){
+  console.log( tabStar[i]+'\n');
+}
