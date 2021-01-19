e291eec164ec52cd4a3231050cbc1102b334caa6
mikikop <michael@cinewan.com>
2021-01-19 09:58:42 +0200

    week2day2Daily fix

--- Week_2/day_2/daily/script.js
+++ Week_2/day_2/daily/script.js
@@ -1,6 +1,7 @@
 // let sentence = 'this dinner is not bad !';
 // let sentence = 'This movie is not so bad!';
-let sentence = 'this dinner is bad !';
+// let sentence = 'this dinner is bad !';
+let sentence = 'this dinner is not so bad, but it\'s cool !';
 
 //first not occurence index
 let notFirstOcc = sentence.indexOf('not');
@@ -11,5 +12,11 @@ if (notFirstOcc == -1 || badFirstOcc == -1 || notFirstOcc > badFirstOcc) {
     alert(sentence);
 } else if (notFirstOcc < badFirstOcc) {
     let newSentence = sentence.substr(0, notFirstOcc - 1);
-    alert(newSentence + ' good !');
+    let endSentence = sentence.substr(badFirstOcc+3,sentence.length);
+    if (endSentence == null) {
+        alert(newSentence + ' good ');
+    } else {
+        alert(newSentence + ' good' + endSentence);
+    }
+    
 }
