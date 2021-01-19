00038e0fdf5ad988b01e68152fb0694c7a079df5
mikikop <michael@cinewan.com>
2021-01-19 17:11:27 +0200

    week2day3 Exercises XP fix

--- Week_2/day_3/exercises/XP/script.js
+++ Week_2/day_3/exercises/XP/script.js
@@ -1,88 +1,77 @@
-//Ex1
-let myFavoriteColors = ["blue", "black", "grey", "yellow"];
-let suffix = {
-  1: "st",
-  2: "nd",
-  3: "rd",
-  4: "th"
-};
+// //Ex1
+// let myFavoriteColors = ["blue", "black", "grey", "yellow"];
+// let suffix = {
+//   1: "st",
+//   2: "nd",
+//   3: "rd",
+//   4: "th"
+// };
 
-for (let i = 0; i < myFavoriteColors.length; i++) {
-  const element = myFavoriteColors[i];
-  if ((i + 1) == 1) {
-    const element = myFavoriteColors[i];
-    console.log('My 1' + suffix["1"] + ' choice is ' + element);
-  } else if ((i + 1) == 2) {
-    const element = myFavoriteColors[i];
-    console.log('My 2' + suffix["2"] + ' choice is ' + element);
-  } else if ((i + 1) == 3) {
-    const element = myFavoriteColors[i];
-    console.log('My 3' + suffix["3"] + ' choice is ' + element);
-  } else {
-    const element = myFavoriteColors[i];
-    console.log('My ' + (i + 1) + suffix["4"] + ' choice is ' + element);
+// for (let i = 0; i < myFavoriteColors.length; i++) {
+//   const element = myFavoriteColors[i];
+//   if ((i + 1) == 1) {
+//     const element = myFavoriteColors[i];
+//     console.log('My 1' + suffix["1"] + ' choice is ' + element);
+//   } else if ((i + 1) == 2) {
+//     const element = myFavoriteColors[i];
+//     console.log('My 2' + suffix["2"] + ' choice is ' + element);
+//   } else if ((i + 1) == 3) {
+//     const element = myFavoriteColors[i];
+//     console.log('My 3' + suffix["3"] + ' choice is ' + element);
+//   } else {
+//     const element = myFavoriteColors[i];
+//     console.log('My ' + (i + 1) + suffix["4"] + ' choice is ' + element);
+//   }
+// }
+
+
+//Ex2
+let people = ["Greg", "Mary", "Devon", "James"];
+let peopleCopy = [];
+//1
+for (const i in people) {
+  if (Object.hasOwnProperty.call(people, i)) {
+    const element = people[i];
+    console.log (element);
   }
 }
+//2
+people.shift();
+//3
+people.splice(2,2,"Jason");
+console.log(people);
+//4
+people.push("Mike");
+console.log(people);
+//5
+for (const i in people) {
+  if (Object.hasOwnProperty.call(people, i)) {
+    const element = people[i];
+    if (element == "Jason") {
+      break;
+    }else {
+      console.log (element);
+    }
+  }
+}
+//6
+peopleCopy = people.slice(1,people.length-1);
+console.log("Copy of array without Mary and my name Mike: " + peopleCopy);
 
+//Same but no matter where Mary and Mike are positioned in the array
+let peopleCopyIn = [];
+let peopleIn = ["Hello", "Mary", "Devon", "Jason", "Mike", "Raoul"]
+console.log(peopleIn);
 
-// for (const i in myFavoriteColors) {
-//   if (Object.hasOwnProperty.call(myFavoriteColors, i)) {
-//     if ((i + 1) == 1) {
-//       const element = myFavoriteColors[i];
-//       console.log('My 1' + suffix["1"] + ' choice is ' + element);
-//     } else if ((i + 1) == 2) {
-//       const element = myFavoriteColors[i];
-//       console.log('My 2' + suffix["2"] + ' choice is ' + element);
-//     } else if ((i + 1) == 3) {
-//       const element = myFavoriteColors[i];
-//       console.log('My 3' + suffix["3"] + ' choice is ' + element);
-//     } else {
-//       const element = myFavoriteColors[i];
-//       console.log('My ' + (i + 1) + suffix["4"] + ' choice is ' + element);
-//     }
-//   }
-// }
-
-
-// //Ex2
-// let people = ["Greg", "Mary", "Devon", "James"];
-// let peopleCopy = [];
-// //1
-// for (const i in people) {
-//   if (Object.hasOwnProperty.call(people, i)) {
-//     const element = people[i];
-//     console.log (element);
-//   }
-// }
-// //2
-// people.shift();
-// //3
-// people.splice(2,2,"Jason");
-// console.log(people);
-// //4
-// people.push("Mike");
-// console.log(people);
-// //5
-// for (const i in people) {
-//   if (Object.hasOwnProperty.call(people, i)) {
-//     const element = people[i];
-//     if (element == "Jason") {
-//       break;
-//     }else {
-//       console.log (element);
-//     }
-//   }
-// }
-// //6
-// for (let i = 0; i < people.length; i++) {
-//   const element = people[i];
-//   if (element == "Mary" || element == "Mike") {
-//     continue;
-//   }else {
-//     peopleCopy.push(element);
-//   }
-// }
-// console.log("Copy of array without Mary and my name Mike: " + peopleCopy);
+for (let i = 0; i < peopleIn.length; i++) {
+  const element = peopleIn[i];
+  if (element == "Mary" || element == "Mike") {
+    continue;
+  }else {
+    peopleCopyIn.push(element);
+  }
+}
+console.log("Copy of array without Mary and my name Mike: " + peopleCopyIn);
 
 // //7
 // console.log("the index of Mary is "+people.indexOf("Mary"));
