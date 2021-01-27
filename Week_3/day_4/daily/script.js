00180cf33db2e00eb9b4c2bc63a6906113af9c43
mikikop <michael@cinewan.com>
2021-01-27 23:53:01 +0200

    week3day5 Daily

--- Week_3/day_4/daily/script.js
+++ Week_3/day_4/daily/script.js
@@ -1,18 +1,18 @@
-let inputHtml = document.getElementById("fname");
-let charValue = inputHtml.value;
-inputHtml.addEventListener("keypress", myFunction);
+let answer = prompt('How many lines of stars do you want?');
+let starLine = Number(answer);
 
-function myFunction(event){
-    //event.preventDefault();
-    if (!isCharALetter(event.value)){
-        console.log("not a letter");
-    }
+let tabStar = [];
+
+//number of lines
+for (let i = 0; i < starLine ; i++) {
+  var starString = '';
+  //number of stars per line
+  for (let j=0; j<= i ; j++){
+    starString = starString + '*'.repeat(j+1);
+  }
+  tabStar[i] = starString;
 }
 
-function isCharALetter (char){
-    if (char>=65 && char<=90 || char>=97 && char<=122){
-        return true;
-    }else{
-        return false;
-    }
+for (let i=0;i<tabStar.length;i++){
+  console.log(" ".repeat(tabStar.length-i-1) + tabStar[i]+ " ".repeat(tabStar.length-i-1) + '\n');
 }
