7b5937b8c9dc9aff0a16586fd5297a3b6adf557b
mikikop <michael@cinewan.com>
2021-02-10 23:04:43 +0200

    Week5 day4 daily

---
+++ Week_5/day_4/daily/codedaily.py
@@ -0,0 +1,118 @@
+from cprint import cprint
+from time import process_time
+import string
+
+# with open ("the_stranger.txt", "r") as f:
+# 	text = f.readlines()
+
+
+class Text():
+
+	def __init__(self, my_string, words = '',word_dict={}):
+		self.my_string = my_string
+		self.words = words
+		self.words += ''.join(self.my_string)
+		self.words = self.words.split()
+		self.word_dict = word_dict
+
+	def __repr__(self):
+		print(self.words)
+	
+	def frequency(self,a_word):
+		count = 0
+		for word in self.words:
+			if word == a_word:
+				count += 1
+		return count
+
+	def most_frequent(self):
+		max_list = []
+
+		for word in self.words:
+			if word not in self.word_dict.keys():
+				self.word_dict[word] = 1
+			else:
+				self.word_dict[word] += 1
+
+		max_occ = max(self.word_dict.values())
+		for key,value in self.word_dict.items():
+			if value == max_occ:
+				max_list.append((key,value))
+
+		return max_list
+
+
+	# def unique_words(self):
+	# 	t1_start = process_time()
+	# 	word_unique_list = []
+
+	# 	# parsing all the text and for each word using the frequency() function
+	# 	# and then parsing again the text to look for occurence == 1
+	# 	# that means we could be not check all the words with more than
+	# 	# 1 occurence. TAKES a REAL LONG TIME 
+	# 	for word in self.words:
+	# 		if self.frequency(word) == 1:
+	# 			word_unique_list.append(word)
+
+	# 	t1_stop = process_time()
+	# 	t_final = t1_stop-t1_start
+	# 	print("Elapsed time:", t1_stop, t1_start)  
+	# 	print("Elapsed time: ",t_final)
+	# 	return word_unique_list
+
+	def unique_words(self):
+		t1_start = process_time()
+		word_unique_list = []
+
+		# for each word in text, if the word is not in the dictionary put it
+		# with occurence 1. If it is already in the list increment the value
+		# Then, in the dictionary look for the values == 1 and put the key value in
+		# a list. 40 times faster
+		for word in self.words:
+			if word not in self.word_dict.keys():
+				self.word_dict[word] = 1
+			else:
+				self.word_dict[word] += 1
+
+		for key,value in self.word_dict.items():
+			if value == 1:
+				word_unique_list.append((key,value))
+
+		t1_stop = process_time()
+		t_final = t1_stop-t1_start
+		#print("Elapsed time:", t1_stop, t1_start)  
+		print("Elapsed time: ",t_final)
+		return word_unique_list
+
+
+	@classmethod
+	def from_file(self, my_file):
+		with open (my_file, "r") as f:
+			text = f.readlines()
+		return Text(text)
+	
+
+
+class TextModification(Text):
+	
+	def __init__(self, my_string, words ='',word_dict={}):
+		super().__init__(self, my_string, words ='',word_dict={})
+	
+	def no_punct(self):
+		new_list = []
+
+		for word in self.words:
+			self.words.append(word.translate(str.maketrans('','',string.punctuation)))
+		return self.words
+
+
+
+
+
+
+
+
+
+
+
+
