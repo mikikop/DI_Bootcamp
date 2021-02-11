c3a15a0e5c4564c7eecd8377f6dba08a615e7892
mikikop <michael@cinewan.com>
2021-02-11 21:26:47 +0200

    Week5 day5 Miniprojet

---
+++ Week_5/day_5/Miniproject/codeminiproj.py
@@ -0,0 +1,221 @@
+allowed_list = [
+	(0,1),
+	(0,3),
+	(0,5),
+	(0,7),
+	(1,0),
+	(1,2),
+	(1,4),
+	(1,6),
+	(2,1),
+	(2,3),
+	(2,5),
+	(2,7),
+	(3,0),
+	(3,2),
+	(3,4),
+	(3,6),
+	(4,1),
+	(4,3),
+	(4,5),
+	(4,7),
+	(5,0),
+	(5,2),
+	(5,4),
+	(5,6),
+	(6,1),
+	(6,3),
+	(6,5),
+	(6,7),
+	(7,0),
+	(7,2),
+	(7,4),
+	(7,6)
+]
+
+
+class Piece:
+	
+	def __init__(self, player,coordonates,name):
+		self.player = player
+		self.coordonates = coordonates
+		self.name = name
+
+	def flip_piece(self):
+		if self.color == 'o':
+			self.color = 'x'
+			return self.color
+		else:
+			self.color = 'o'
+			return self.color
+
+	def __repr__(self):
+		return f'{self.name}'
+
+class Game:
+	
+	def __init__(self, allowed_list):
+		self.allowed_list = allowed_list
+		self.p1 = Player('x')
+		self.p2 = Player('o')
+		self.pieces = []
+		self.board = {key: None for key in allowed_list}
+		for player in [self.p1, self.p2]:
+			if self.pieces:
+				coo_list = allowed_list[::-1]
+			else:
+				coo_list = allowed_list
+			for i,coo in zip(range(1,13), coo_list):
+				piece = Piece(player,coo,player.color+str(i))
+				self.pieces.append(piece)
+				self.board[coo] = piece
+		
+		#print(self.pieces)		
+		print(self.board)
+
+		
+
+
+
+class Player:
+	
+	def __init__(self, color):
+		self.color = color
+
+	def move(self,from_pos, to_pos):
+		to_pos_int = eval(to_pos)
+		from_pos_int = eval(from_pos)
+
+		if to_pos_int not in coordonates_allowed:
+			print("you entered wrong coordonates!")
+			exit()		
+		for key,value in board_game.items():
+			if key == to_pos_int:
+				if value == 0:
+					board_game[key] = 1
+					board_game[from_pos] = 0
+				else:
+					# from_pos is like (0,3) and to_pos is like (1,4)
+					if from_pos_int[1]<to_pos_int[1]:
+						if board_game[to_pos_int[0]+1][to_pos_int[1]+1] == 0:
+							board_game[to_pos_int[0]+1][to_pos_int[1]+1] = 1
+							board_game[from_pos_int] = 0
+							board_game[to_pos_int] = 0
+					# from_pos is like (0,3) and to_pos is like (1,2)
+					else:
+						if board_game[to_pos_int[0]+1][to_pos_int[1]-1] == 0:
+							board_game[to_pos_int[0]+1][to_pos_int[1]-1] = 1
+							board_game[from_pos_int] = 0
+							board_game[to_pos_int] = 0
+								
+
+
+
+if __name__ == '__main__':
+	#main()
+	board = Game(allowed_list)
+
+
+	from_pos = '(0,3)'
+	to_pos = '(1,4)'
+	p1 = Player('x')
+	print(board)
+	p1.move(from_pos,to_pos)
+	print(board)
+
+
+
+
+
+# board_game = [
+# 	[(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)],
+# 	[(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)],
+# 	[(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7)],
+# 	[(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7)],
+# 	[(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7)],
+# 	[(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7)],
+# 	[(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7)],
+# 	[(7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7)],
+# ]
+
+# board = [
+# 	['',1,'',1,'',1,'',1],
+# 	[1,'',1,'',1,'',1,''],
+# 	['',1,'',1,'',1,'',1],
+# 	[0,'',0,'',0,'',0,''],
+# 	['',0,'',0,'',0,'',0],
+# 	[1,'',1,'',1,'',1,''],
+# 	['',1,'',1,'',1,'',1],
+# 	[1,'',1,'',1,'',1,'']
+# ]
+
+# board_game = {
+# 	'(0,1)',
+# 	'(0,3)',
+# 	'(0,5)': 1,
+# 	'(0,7)': 1,
+# 	'(1,0)': 1,
+# 	'(1,2)': 1,
+# 	'(1,4)': 1,
+# 	'(1,6)': 1,
+# 	'(2,1)': 1,
+# 	'(2,3)': 1,
+# 	'(2,5)': 1,
+# 	'(2,7)': 1,
+# 	'(3,0)': 0,
+# 	'(3,2)': 0,
+# 	'(3,4)': 0,
+# 	'(3,6)': 0,
+# 	'(4,1)': 0,
+# 	'(4,3)': 0,
+# 	'(4,5)': 0,
+# 	'(4,7)': 0,
+# 	'(5,0)': 1,
+# 	'(5,2)': 1,
+# 	'(5,4)': 1,
+# 	'(5,6)': 1,
+# 	'(6,1)': 1,
+# 	'(6,3)': 1,
+# 	'(6,5)': 1,
+# 	'(6,7)': 1,
+# 	'(7,0)': 1,
+# 	'(7,2)': 1,
+# 	'(7,4)': 1,
+# 	'(7,6)': 1
+# }
+
+# board_game = {
+# 	(0,1): 1,
+# 	(0,3): 1,
+# 	(0,5): 1,
+# 	(0,7): 1,
+# 	(1,0): 1,
+# 	(1,2): 1,
+# 	(1,4): 1,
+# 	(1,6): 1,
+# 	(2,1): 1,
+# 	(2,3): 1,
+# 	(2,5): 1,
+# 	(2,7): 1,
+# 	(3,0): 0,
+# 	(3,2): 0,
+# 	(3,4): 0,
+# 	(3,6): 0,
+# 	(4,1): 0,
+# 	(4,3): 0,
+# 	(4,5): 0,
+# 	(4,7): 0,
+# 	(5,0): 1,
+# 	(5,2): 1,
+# 	(5,4): 1,
+# 	(5,6): 1,
+# 	(6,1): 1,
+# 	(6,3): 1,
+# 	(6,5): 1,
+# 	(6,7): 1,
+# 	(7,0): 1,
+# 	(7,2): 1,
+# 	(7,4): 1,
+# 	(7,6): 1
+# }
+# 		
